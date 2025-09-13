bc6df2850f61fa26966cddd99357181d9926eee6
zyon-king <clt.cleiton@gmail.com>
2025-09-13 16:19:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const { Appwrite } = require('appwrite');
+// Remove esta linha: const { Appwrite } = require('appwrite');
 
 module.exports = async ({ req, res, log, error }) => {
   // 1. Pega o token enviado pelo site
   const tokenFornecido = req.body.token;
-
   // 2. Pega o token secreto da variável de ambiente
   const tokenSecreto = process.env.TOKEN_SECRETO;
-
   // 3. Compara os dois tokens
   if (tokenFornecido === tokenSecreto) {
     // 4. Se forem iguais, envia uma resposta de sucesso
